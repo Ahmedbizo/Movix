0f183807c9a2b4e5c0cf95413a999bb8d31c215d
Ahmedbizo <ahmedmajedb@hotmail.com>
2021-09-29 23:10:54 +0200

    added search screen to the paa

---
+++ screens/search.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+
+
+const search = ({navigation}) => {
+    return (
+        <React.Fragment>
+        
+        <SafeAreaView>
+        
+        
+        </SafeAreaView>
+
+        </React.Fragment>
+    );
+}
+
+export default search;
